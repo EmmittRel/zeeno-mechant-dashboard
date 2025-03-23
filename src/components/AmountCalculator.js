675c6d1cc0124a3770afff405922c41bb24f71b2
Pramit Bakhrel <bakhrelpramit07@gmail.com>
2025-03-23 21:25:50 +0545

    fix: international votes

--- src/components/AmountCalculator.js
+++ src/components/AmountCalculator.js
@@ -1,41 +1,40 @@
-// AmountCalculator.js
 export const calculateVotes = (amount, currency) => {
-    // Currency mapping
-    const currencyValues = {
-      USD: 10,
-      AUD: 5,
-      GBP: 10,
-      CAD: 5,
-      EUR: 10,
-      AED: 2,
-      QAR: 2,
-      MYR: 2,
-      KWD: 2,
-      HKD: 1, 
-      CNY: 1,
-      SAR: 2,
-      OMR: 20,
-      SGD: 8,
-      NOK: 1,
-      KRW: 200, 
-      JPY: 20,
-      THB: 4,
-      INR: 10,
-      NPR: 10,
-    };
-  
-    const currencyValue = currencyValues[currency] || 1;
-  
-    let votes;
-    if (currency === 'HKD') {
-      votes = Math.floor(amount); 
-    } else if (['JPY', 'THB', 'INR', 'NPR'].includes(currency)) {
-      votes = Math.floor(amount / currencyValue);
-    } else if (currency === 'KRW') {
-      votes = Math.floor(amount / 200); 
-    } else {
-      votes = Math.floor(amount * currencyValue);
-    }
-  
-    return votes;
+  // Currency mapping
+  const currencyValues = {
+    USD: 10,
+    AUD: 5,
+    GBP: 10,
+    CAD: 5,
+    EUR: 10,
+    AED: 2,
+    QAR: 2,
+    MYR: 2,
+    KWD: 20,
+    HKD: 1, 
+    CNY: 1,
+    SAR: 2,
+    OMR: 20,
+    SGD: 8,
+    NOK: 1,
+    NZD: 4,
+    ILS: 2,
+    KGS: 1,
+    KRW: 1 / 200,
+    BDT: 1 / 15,
+    INR: 1 / 10,
+    NPR: 1 / 10,
+    JPY: 1 / 20,
+    THB: 1 / 4
   };
+
+  const currencyValue = currencyValues[currency] || 1;
+
+  let votes;
+  if (currency === 'HKD') {
+    votes = Math.floor(amount); 
+  } else {
+    votes = Math.floor(amount * currencyValue);
+  }
+
+  return votes;
+};
